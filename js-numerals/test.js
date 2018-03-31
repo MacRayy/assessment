9aa3d6bef9f43f9824d1ce82c5e13f082b13c9a8
Zsolt Makrai <zsolt.makrai@blackswan.com>
2018-03-31 20:03:00 +0200

    test(converter): create test for the number 1

--- js-numerals/test.js
+++ js-numerals/test.js
@@ -1,8 +1,14 @@
 'use strict'
 
 import test from 'tape'
+import NumberToStringConverter from './converter.js'
 
 test('A passing test', (t) => {
 	t.pass('This test will pass.')
+	t.end()
+})
+
+test('Test for one digit number', (t) => {
+	t.equal(NumberToStringConverter.getWord(1), 'one')
 	t.end()
 })
